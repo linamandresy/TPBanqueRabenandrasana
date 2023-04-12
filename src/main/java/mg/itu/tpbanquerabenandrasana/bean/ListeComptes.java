7046d49ff6dbae19901269ced61103a2cc49cdd5
Lina Rabenandrasana <mandresyrab@gmail.com>
2023-04-13 00:27:00 +0300

    Page qui liste tous les comptes

---
+++ src/main/java/mg/itu/tpbanquerabenandrasana/bean/ListeComptes.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.tpbanquerabenandrasana.bean;
+
+import jakarta.ejb.EJB;
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import java.io.Serializable;
+import java.util.List;
+import mg.itu.tpbanquerabenandrasana.ejb.GestionnaireCompte;
+import mg.itu.tpbanquerabenandrasana.entities.CompteBancaire;
+
+/**
+ *
+ * @author mandr
+ */
+@Named(value = "listeComptes")
+@ViewScoped
+public class ListeComptes implements Serializable {
+
+    @EJB
+    GestionnaireCompte gc;
+    
+    private List<CompteBancaire> allComptes;
+    /**
+     * Creates a new instance of ListeComptes
+     */
+    public ListeComptes() {
+    }
+
+    public List<CompteBancaire> getAllComptes() {
+        if(allComptes==null){
+            allComptes = gc.getAllComptes();
+        }
+        return allComptes;
+    }
+
+    
+    
+}
